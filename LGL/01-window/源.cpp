2323d5e99ff4a40530a1afd2a9e9307bfd4dafd2
ZhengDianxin <zhengdianxin1999@gmail.com>
2021-10-07 16:03:52 +0800

    Window with color change

--- LGL/01-window/源.cpp
+++ LGL/01-window/源.cpp
@@ -11,17 +11,20 @@ class my_application : public sb7::application
 public:
 
 	// Our rendering function
+	//void render(double currentTime)
+	//{
+	//	// Simply clear the window with red
+	//	static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+	//	glClearBufferfv(GL_COLOR, 0, red);
+	//}
 
+	// Our rendering function
 	void render(double currentTime)
-
 	{
-
-		// Simply clear the window with red
-
-		static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-
-		glClearBufferfv(GL_COLOR, 0, red);
-
+		const GLfloat color[] = { (float)sin(currentTime) * 0.5f + 0.5f,
+		(float)cos(currentTime) * 0.5f + 0.5f,
+		0.0f, 1.0f };
+		glClearBufferfv(GL_COLOR, 0, color);
 	}
 
 };
